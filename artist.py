c03a9af72cdb2293c23c43634cf325374a76f020
B33 <my@email.com>
2020-02-09 15:08:05 -0600

    Add methods for interacting with the Artist table

--- artist.py
+++ artist.py
@@ -1,1 +1,13 @@
-class Artist():
+from model import *
+
+def add_artist(artist):
+    try:
+        artist.save()
+        return True
+    except peewee.IntegrityError:
+        return False
+
+def show_artists_art(artist_id):
+    artist = Artist.select().where(Artist.id == artist_id)
+    query = Art.select(Art).join(Art).where(Art.artist = artist.name)
+    return list(query)

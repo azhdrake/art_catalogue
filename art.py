a9738742494e464e6c997f8983f006a1586664e2
B33 <my@email.com>
2020-02-09 14:58:54 -0600

    Add methods for interacting with the Art table

--- art.py
+++ art.py
@@ -1,1 +1,31 @@
-class Art:
+from model import *
+
+def add_art(art):
+    try:
+        art.save()
+        return True
+    except peewee.IntegrityError:
+        return False
+
+def show_all_art():
+    query = Art.select()
+    return list(query)
+
+def change_available(art_id, availability):
+    art = get_art_by_id(art_id)
+    if not art:
+        return False
+    art.available = availability
+    art.save()
+    return True)
+
+def delete_art(art):
+    rows_deleted = Art.delete().where(Art.id == art.id).execute()
+    if rows_deleted == 0:
+        raise ArtError('Error deleting art piece.')
+
+def get_art_by_id(art_id):
+    return Art.get_or_none(Art.id == art_id)
+
+class ArtError(Exception):
+    pass

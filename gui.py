7ed89408ddaa439835f225847fbe68a3847065f7
B33 <my@email.com>
2020-02-10 07:20:17 -0600

    Impliment Flask UI

---
+++ gui.py
@@ -0,0 +1,2 @@
+from main import app
+from flask import render_template
---
+++ main.py
@@ -0,0 +1,11 @@
+from menu import Menu
+from art import Art
+from artist import Artist
+import art
+import artist
+import ui
+from flask import Flask
+
+app = Flask(__name__)
+
+import gui

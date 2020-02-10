506ad042ced6b44d2b650b2440dcf6a0f2b01849
B33 <my@email.com>
2020-02-10 07:21:59 -0600

    Add art desplay page

--- gui.py
+++ gui.py
@@ -1,2 +1,9 @@
 from main import app
 from flask import render_template
+import art
+
+@app.route('/')
+@app.route('/art')
+def show_art():
+    art_list = art.get_all_art()
+    return render_template('desplay_art.html', art_list = art_list)

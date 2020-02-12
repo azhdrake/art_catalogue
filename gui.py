0dee356f369bad2cc0e2d5d5f6503db7efd6df79
B33 <my@email.com>
2020-02-12 09:32:35 -0600

    Made delete and change status buttons both work on display_art page

--- gui.py
+++ gui.py
@@ -6,6 +6,9 @@ import artist
 from artist import Artist
 from get_info import ArtInputForm, ArtistInputForm
 
+# Most of my Flask knowledge was gained using these tutorials: https://www.youtube.com/playlist?list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH
+
+# routes gives the url ending and method the page will accept. Method under route is what runs when that route is reached.
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/art', methods=['GET', 'POST'])
 @app.route('/addart', methods=['GET', 'POST'])
@@ -34,21 +37,22 @@ def addartist():
 @app.route('/delete_art', methods=['POST'])
 def delete_art():
     art_form = ArtInputForm()
-    if request.method == 'POST':
+    if request.form['submit_button'] == 'delete':
         id_list = request.form.getlist('selected')
         for art_id in id_list:
             art.delete_art(art_id)
         flash('The art has been deleted!', 'success')
+    elif request.form['submit_button'] == 'change':
+        id_list = request.form.getlist('selected')
+        for art_id in id_list:
+            art.change_available(art_id)
+        flash('The deed is done!', 'success')
     art_list = art.get_all_art()
     return render_template('add_art.html', title='AddArt', form=art_form, art_list=art_list)
 
 @app.route('/change_status', methods=['POST'])
 def change_status():
     art_form = ArtInputForm()
-    if request.method == 'POST':
-        id_list = request.form.getlist('selected')
-        for art_id in id_list:
-            art.change_available(art_id)
-        flash('The deed is done!', 'success')
+    
     art_list = art.get_all_art()
     return render_template('add_art.html', title='AddArt', form=art_form, art_list=art_list)
